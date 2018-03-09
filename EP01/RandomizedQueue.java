e959b99255ed5ab84f244aa61852b685e6d398c2
Zeke <vitinho_cga@hotmail.com>
2018-03-09 10:16:14 -0300

    Adds a iterator for the randomized queue.

--- EP01/RandomizedQueue.java
+++ EP01/RandomizedQueue.java
@@ -1,9 +1,12 @@
 import java.util.Iterator;
-import stdRamdom.uniform;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private int last;
     private int lenth;
+    private Item[] queue;
     //private int ocupied;
 
     // construct an empty randomized queue
@@ -11,7 +14,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         this.last = -1;
         this.lenth = 1;
         //this.ocupied = 0;
-        Item[] queue = (Item[]) new object[1];
+        this.queue = (Item[]) new Object[1];
     }
     // is the randomized queue empty?
     public boolean isEmpty() {
@@ -24,40 +27,97 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     // add the item
     public void enqueue(Item item) {
         if (this.last == this.lenth)
-            queue.resize();
+            this.resizeUp();
         this.last += 1;
-        queue[last] = item;
+        this.queue[last] = item;
         //this.ocupied += 1;
     }
     // remove and return a random item
-    public Item dequeue() {
-        int toRemove = srtRamdom.uniform(this.last + 1);
-        Item temp = queue[toRemove];
-        queue[toRemove] = queue[this.last];
+    public Item  dequeue() {
+        int toRemove = StdRandom.uniform(this.last + 1);
+        Item temp = this.queue[toRemove];
+        this.queue[toRemove] = this.queue[this.last];
         this.last -= 1;
         return temp;
     }
     // return a random item (but do not remove it)
     public Item sample() {
-        return(queue[srtRamdom.uniform(this.last + 1)]);
+        return(this.queue[StdRandom.uniform(this.last + 1)]);
     }
 
-    private resize() {
-        Item[] newQueue = (Item[]) new object[this.lenth*2];
+    private void resizeUp() {
+        Item[] newQueue = (Item[]) new Object[this.lenth*2];
         for (int i = 0; i < this.last + 1; i++)
-            newQueue[i] = queue[i];
-        queue = newQueue;
+            newQueue[i] = this.queue[i];
+        this.queue = newQueue;
         this.lenth *= 2;
         return;
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return new queueIterator();
+        return new QueueIterator();
+    }
+
+    private class QueueIterator implements Iterator<Item>
+    {
+        int current;
+        public QueueIterator() {
+            this.current = -1;
+        }
+
+        public boolean hasNext() {
+            return this.current <= last;
+        }
+
+        public Item next() {
+            if (!this.hasNext())
+                    throw new java.util.NoSuchElementException("RandomizedQueue has no elements left.\n");
+            this.current += 1;
+            return queue[this.current];
+        }
+
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException("Invalid operation.\n");
+        }
     }
 
     // unit testing (required)
     public static void main(String[] args) {
+        RandomizedQueue<String> testQueue = new RandomizedQueue<String>();
+        StdOut.print("Is the Randomized Queue empty ? " + testQueue.isEmpty() + "\n");
+        StdOut.print("What is the size of the Randomized Queue ? " + testQueue.size() + "\n");
+
+        //Testing the addition to the Randomized Queue.
+        StdOut.print("Please type 10 inputs for the Randomized Queue.\n");
+        for (int i = 0; i < 10; i++) {
+            String word = StdIn.readString();
+            testQueue.enqueue(word);
+        }
+        StdOut.print("is the Randomized Queue empty ? " + testQueue.isEmpty() + "\n");
+        StdOut.print("What is the size of the Randomized Queue ? " + testQueue.size() + "\n");
+
+        //Testing the iterator.
+        StdOut.print("Now we will test the iterator.\n");
+        Iterator<String> testIterator = testQueue.iterator();
+        for (int i = 0; i < 10; i++) {
+            if (testIterator.hasNext())
+                StdOut.print(testIterator.next() + " ");
+        }
+        StdOut.print("\n");
+
+        //Testing the removal from the end and the begginig of the Randomized Queue.
+        StdOut.print("Now we will test the removal from the Randomized Queue.\n");
+        for (int i = 0; i < 10; i++) {
+            if (i%2 == 0)
+                StdOut.print(testQueue.removeFirst() + " ");
+            else
+                StdOut.print(testQueue.removeLast() + " ");
+        }
+        StdOut.print("\n");
+
+        StdOut.print("Testar a exception do remove().\n");
+        testIterator.remove();
 
     }
 }
