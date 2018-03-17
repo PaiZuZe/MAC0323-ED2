1ba65a57f47f19043a204fc9bca0b214a09c5778
PaiZuZe <vitinho_cga@hotmail.com>
2018-03-17 15:35:49 -0300

    Adds a test.

--- EP02/PercolationStats.java
+++ EP02/PercolationStats.java
@@ -1,29 +1,23 @@
 import java.lang.Math;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-
-
+import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
     private int numTrials;
-    private double samMean;
-    private double samStddev;
-    private int[] openSites;
+    private double[] openSites;
     private Percolation bob;
 
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         this.numTrials = trials;
-        this.samMean = 0;
-        this.samStddev = 0;
-        this.openSites = new int[n];
+        this.openSites = new double[trials];
 
         for (int i = 0; i < trials; i++) {
             this.bob = new Percolation(n);
             simulation(n, i);
         }
-
     }
 
     // sample mean of percolation threshold
@@ -49,12 +43,16 @@ public class PercolationStats {
     private void simulation(int n, int count) {
         while (!this.bob.percolates())
             this.bob.open(StdRandom.uniform(n), StdRandom.uniform(n));
-        this.openSites[count] = bob.numberOfOpenSites();
+        this.openSites[count] = bob.numberOfOpenSites()/ (double) (n*n);
         return;
     }
 
     // test client (see below)
     public static void main(String[] args) {
-
+        PercolationStats test = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        StdOut.print("mean: " + test.mean() + " ");
+        StdOut.print("stddev: " + test.stddev() + " ");
+        StdOut.print("confidenceLow: " + test.confidenceLow() + " ");
+        StdOut.print("confidenceHigh: " + test.confidenceHigh() + "\n");
     }
 }
