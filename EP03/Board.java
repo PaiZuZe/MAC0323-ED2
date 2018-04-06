2866c1f5b85792d91a5e7944f5df76505c8973f1
Zeke <vitinho_cga@hotmail.com>
2018-04-06 10:09:20 -0300

    Adds compare. -Z

--- EP03/Board.java
+++ EP03/Board.java
@@ -10,6 +10,29 @@ public class Board {
         return math.abs(x - x_value) + math.abs(y - y_value);
     }
 
+    private class BoardIterator implements Iterator<Board> {
+        private Node current;
+
+        public DequeIterator() {
+            this.current = first;
+        }
+
+        public boolean hasNext() {
+            return this.current != null;
+        }
+        public Item next() {
+            if (!hasNext())
+                throw new java.util.NoSuchElementException("Board has no neighbors left.\n");
+            Node temp = this.current;
+            this.current = this.current.next;
+            return temp.data;
+        }
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException("Invalid operation.\n");
+        }
+    }
+
+
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
@@ -20,6 +43,7 @@ public class Board {
                 thos.board[i][j] = tiles[i][j];
         return;
     }
+
     // string representation of this board
     public String toString() {
         String s_matrix = this.n + "\n";
@@ -30,31 +54,40 @@ public class Board {
         }
         return s_matrix;
     }
+
     // tile at (row, col) or 0 if blank
     public int tileAt(int row, int col) {
+        if (0 <= row < this.n or 0 <= col < this.n)
+            throw new java.lang.IllegalArgumentException("col or row are not in the right range.\n");
         return this.board[row][col];
     }
+
     // board size n
     public int size() {
         return this.n;
     }
+
     // number of tiles out of place
     public int hamming() {
         int out_tiles = 0;
         for (int i = 0; i < this.n; i++)
             for (int j = 0; j < this.n; j++)
-                if (i * this.size + j + 1 != this.board[i][j])
+                if (i * this.size + j + 1 != this.board[i][j] and this.board[i][j] != 0)
                     out_tiles += 1;
         return out_tiles;
     }
+
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
         int tot_manha = 0;
         for (int i = 0; i < this.n; i++)
             for (int j = 0; j < this.n; j++)
+                if (this.board[i][j] == 0)
+                    continue;
                 tot_manha += single_manhathan(i, j);
         return tot_manha;
     }
+
     // is this board the goal board?
     public boolean isGoal() {
         for (int i = 0; i < this.n; i++)
@@ -63,15 +96,22 @@ public class Board {
                     return false;
         return true;
     }
+
      // does this board equal y?
     public boolean equals(Object y) {
-        
+        if (this == y) return true;
+        if (y == null) return false;
+        if (getClass() != y.getClass()) return false;
+        Board other_board = (Board) y;
+        return Objects.equals(this.n, other_board.n) && Objects.equals(this.board, other_board.board);
 
     }
+
     // all neighboring boards
     public Iterable<Board> neighbors() {
 
     }
+
     // is this board solvable?
     public boolean isSolvable() {
 
