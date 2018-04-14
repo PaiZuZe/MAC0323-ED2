0f09eea874c01ffcd238d7f53eeee118ee287fba
PaiZuZe <vitinho_cga@hotmail.com>
2018-04-13 22:27:39 -0300

    Initial work on Solver. -B

--- EP03/Solver.java
+++ EP03/Solver.java
@@ -1,16 +1,42 @@
+
 public class Solver {
-     // find a solution to the initial board (using the A* algorithm)
+    private int mim_moves;
+
+    private class Node implements Comparable<Node> {
+        private Board board;
+        private Node(Board boar) {
+            this.board = boar;
+        }
+        @Override
+        public int compareTo(Node otherNode) {
+            if (this.board.manhattan() == otherNode.board.manhattan())
+                return 0;
+            else if (this.board.manhattan() < otherNode.board.manhattan())
+                return -1;
+            else
+                return 1;
+        }
+    }
+
+    // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        if (initial == null)
+            throw new java.lang.IllegalArgumentException("Board can't be null.\n");
+        if (!initial.isSolvable())
+            throw new java.lang.IllegalArgumentException("Board can't be solved.\n");
 
     }
+
     // min number of moves to solve initial board
     public int moves() {
-
+        return mim_moves;
     }
+
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-
+        return null;
     }
+
     // test client (see below)
     public static void main(String[] args) {
 
