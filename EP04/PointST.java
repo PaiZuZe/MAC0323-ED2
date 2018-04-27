01cfca2efdf720499297480ffbbfbadc272fb9cd
Zeke <vitinho_cga@hotmail.com>
2018-04-27 12:54:19 -0300

    Finishes, without testing, the redBlack implemantation. -Z

--- EP04/PointST.java
+++ EP04/PointST.java
@@ -1,13 +1,38 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.MaxPQ;
 import edu.princeton.cs.algs4.Queue;
 
 public class PointST<Value> {
     private RedBlackBST<Point2D, Value> ST;
+    private MaxPQ<Point2D> kNearest;
     private Queue<Point2D> inRect;
+
+    public class Point2Dcomparator implements Comparator<Point2D> {
+        private Point2D origin;
+        public Point2Dcomparator(Point2D org) {
+            origin = org;
+            return;
+        }
+
+        @Override
+        public int compare(Point2D a, Point2D b) {
+            if (a.distanceSquaredTo(origin) == b.distanceSquaredTo(origin))
+                return 0;
+            else if (a.distanceSquaredTo(origin) < b.distanceSquaredTo(origin))
+                return -1;
+            else
+                return 1;
+        }
+    }
+
 
     // construct an empty symbol table of points
     public PointST() {
-        return this.ST = new RedBlackBST<Point2D, Value>();
+        this.ST = new RedBlackBST<Point2D, Value>();
+        return;
     }
 
     // is the symbol table empty?
@@ -44,7 +69,7 @@ public class PointST<Value> {
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
         this.inRect = new Queue<Point2D>();
-        for (Point2D temp : this.ST)
+        for (Point2D temp : this.ST.keys())
             if (rect.contains(temp))
                 this.inRect.enqueue(temp);
         return this.inRect;
@@ -54,15 +79,32 @@ public class PointST<Value> {
     public Point2D nearest(Point2D p) {
         if (this.isEmpty())
             return null;
-        Point2D closer = p;
-        for (Point2D temp : this.ST)
-            if (p.equals(closser) || p.distanceSquaredTo(temp) < p.distanceSquaredTo(closer))
-                closer = temp;
-        return closer;
+        Point2D closest = p;
+        for (Point2D temp : this.ST.keys())
+            if (p.equals(closest) || p.distanceSquaredTo(temp) < p.distanceSquaredTo(closest))
+                closest = temp;
+        return closest;
     }
 
     public Iterable<Point2D> nearest(Point2D p, int k) {
-        return null;
+        if (this.isEmpty())
+            return null;
+        this.kNearest = new MaxPQ<Point2D>(new Point2Dcomparator(p));
+        for (Point2D temp : this.ST.keys()) {
+            if (temp.equals(p))
+                continue;
+
+            if (this.kNearest.size() <= k) {
+                kNearest.insert(temp);
+                continue;
+            }
+
+            if (kNearest.max().distanceSquaredTo(p) > temp.distanceSquaredTo(p)) {
+                kNearest.insert(temp);
+                kNearest.delMax();
+            }
+        }
+        return kNearest;
     }
 
     // unit testing (required)
